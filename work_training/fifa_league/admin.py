a3a18b162c1c68edb0eed22be278b8a3b1df601b
vmois <vladyslav.moisieienkov@gmail.com>
2016-12-27 15:39:41 +0100

    feat: Add new feature to fifa_legue app in Django admin v1.0
    - add search field by names and shortcut, add TeamStat inline to League
    - add search field by names and filter by teams to Player
    - add Player inline to Team
    - add  filter by league, home and guest matches inline to TeamStat

--- work_training/fifa_league/admin.py
+++ work_training/fifa_league/admin.py
@@ -1,9 +1,117 @@
+"""
+admin.py - config file for Django admin panel
+"""
 from django.contrib import admin
+from .models import League, Team, Player, TeamStat, Match
 
-# Register your models here.
-from .models import League, Team, Player, TeamStat
 
-admin.site.register(League)
-admin.site.register(Team)
-admin.site.register(Player)
-admin.site.register(TeamStat)
+admin.site.site_title = "Football App"
+admin.site.site_header = "FIFA Leagues App"
+
+
+class HomeMatchInline(admin.TabularInline):
+    """
+    Home Match class for add to TeamStat inline
+    """
+    model = Match
+    fk_name = "team_home"
+
+
+class GuestMatchInline(admin.TabularInline):
+    """
+    Guest Match class for add to TeamStat inline
+    """
+    model = Match
+    fk_name = "team_guest"
+
+
+class PlayerInline(admin.StackedInline):
+    """
+    Stacked Inline class for Player model
+    """
+    model = Player
+
+
+class TeamStatToLeagueInline(admin.TabularInline):
+    """
+    Tabular Inline class for TeamStat model with foreign key connect to league
+    """
+    model = TeamStat
+    fk_name = 'league'
+
+
+class TeamAdmin(admin.ModelAdmin):
+    """
+    Add PlayerInline to Team inlines in Django admin
+    Display:
+    - Team name column
+
+    Inlines:
+    - Player model connected to this Team
+    """
+    list_display = ['name']
+    inlines = [
+        PlayerInline,
+    ]
+
+
+class TeamStatAdmin(admin.ModelAdmin):
+    """
+    Config class for TeamStat display in Django admin
+    Display:
+    - __str__ name like in Models
+    - points
+
+    Add filter by:
+    - leagues
+
+    Order - order by points, from max to min
+    Inlines:
+    - HomeMatches connected to this TeamStat
+    - GuestMatches connected to this TeamStat
+    """
+    list_display = ['__str__', 'points']
+    list_filter = ['league']
+    ordering = ['-points']
+    inlines = [
+        HomeMatchInline, GuestMatchInline
+    ]
+
+
+class LeagueAdmin(admin.ModelAdmin):
+    """
+    Add TeamStatToLeagueInline to League inlines in Django admin
+    Search by:
+    - name
+    - shortcut
+
+    Inlines:
+    - TeamStats connected to this League
+    """
+    search_fields = ['name', 'shortcut']
+    inlines = [
+        TeamStatToLeagueInline,
+    ]
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    """
+    Player model for Django admin
+    Display:
+    - name column
+
+    Search by:
+    - name
+
+    Add filter by:
+    - teams
+    """
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['team']
+
+
+admin.site.register(League, LeagueAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(TeamStat, TeamStatAdmin)
