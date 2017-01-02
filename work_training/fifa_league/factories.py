453f218cd5ba9d5b9a8e44c458b17400da7633c4
vmois <vladyslav.moisieienkov@gmail.com>
2017-01-02 16:43:07 +0100

    test: Add new tests to tests.py, install coverage and factory_boy
    - add factory class insted default model creation
    - create test for all models
    - add requirements

---
+++ work_training/fifa_league/factories.py
@@ -0,0 +1,55 @@
+"""
+factories.py - file that contains factory classes for tests
+"""
+import factory
+from .models import Match, League, Team, TeamStat, Player
+
+
+class LeagueFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = League
+
+    name = 'TESTLEAGUE'
+    shortcut = 'testleague'
+
+
+class TeamFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Team
+
+    name = factory.Sequence(lambda n: 'TESTTEAM%d' % n)
+    shortcut = factory.Sequence(lambda n: 'testteam%d' % n)
+
+
+class TeamStatFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = TeamStat
+
+    league = LeagueFactory()
+    team = TeamFactory()
+    match_count = 0
+    wins = 0
+    loses = 0
+    draws = 0
+    goals_scored = 0
+    goals_conceded = 0
+    points = 0
+
+
+class MatchFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Match
+
+    team_home = TeamStatFactory()
+    team_guest = TeamStatFactory()
+    team_home_goals = 0
+    team_guest_goals = 0
+
+
+class PlayerFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Player
+
+    team = TeamFactory()
+    name = 'Rocko Pocko'
+    age = 21
--- work_training/fifa_league/tests.py
+++ work_training/fifa_league/tests.py
@@ -2,7 +2,8 @@
 tests.py | File that contains tests for fifa_league app
 """
 from django.test import TestCase
-from .models import Match, League, Team, TeamStat
+from .factories import TeamFactory, LeagueFactory, TeamStatFactory, \
+    PlayerFactory, MatchFactory
 
 
 class MatchCreateTestCase(TestCase):
@@ -12,56 +13,32 @@ class MatchCreateTestCase(TestCase):
     """
 
     def setUp(self):
-        league = League.objects.create(name='TESTCASELEAGUE',
-                                       shortcut='testcaseleague')
-        team1 = Team.objects.create(name='TESTCASETEAM1',
-                                    shortcut='testcaseteam1')
-        team2 = Team.objects.create(name='TESTCASETEAM2',
-                                    shortcut='testcaseteam2')
-
-        self.team_stat1 = TeamStat.objects.create(league=league, team=team1)
-        self.team_stat2 = TeamStat.objects.create(league=league, team=team2)
-
-    def clean_teams_statistic(self):
-        """
-        Clean teams statistics (set all statistic variables to 0)
-        :return:
-        """
-        statistic_list = [self.team_stat1, self.team_stat2]
-        for stat in statistic_list:
-            stat.match_count = 0
-            stat.wins = 0
-            stat.loses = 0
-            stat.draws = 0
-            stat.goals_scored = 0
-            stat.goals_conceded = 0
-            stat.points = 0
+        self.team_stat1 = TeamStatFactory()
+        self.team_stat2 = TeamStatFactory()
 
     def test_team_home_win_statistic(self):
         """
         Test when home team wins the guest team (example 2:1)
         :return:
         """
-        Match.objects.create(team_home=self.team_stat1,
-                             team_guest=self.team_stat2,
-                             team_home_goals=2,
-                             team_guest_goals=1)
+        MatchFactory(team_home=self.team_stat1,
+                     team_guest=self.team_stat2,
+                     team_home_goals=2,
+                     team_guest_goals=1)
         self.assertEqual(self.team_stat1.points, 3)
         self.assertEqual(self.team_stat2.points, 0)
-        self.clean_teams_statistic()
 
     def test_team_home_lose_statistic(self):
         """
         Test when home team loses to guest team (example 1:2)
         :return:
         """
-        Match.objects.create(team_home=self.team_stat1,
-                             team_guest=self.team_stat2,
-                             team_home_goals=1,
-                             team_guest_goals=2)
+        MatchFactory(team_home=self.team_stat1,
+                     team_guest=self.team_stat2,
+                     team_home_goals=1,
+                     team_guest_goals=2)
         self.assertEqual(self.team_stat1.points, 0)
         self.assertEqual(self.team_stat2.points, 3)
-        self.clean_teams_statistic()
 
     def test_team_draw_statistic(self):
         """
@@ -69,9 +46,73 @@ class MatchCreateTestCase(TestCase):
         have equal count of goals (example 1:1)
         :return:
         """
-        Match.objects.create(team_home=self.team_stat1,
-                             team_guest=self.team_stat2,
-                             team_home_goals=1,
-                             team_guest_goals=1)
+        MatchFactory(team_home=self.team_stat1,
+                     team_guest=self.team_stat2,
+                     team_home_goals=1,
+                     team_guest_goals=1)
         self.assertEqual(self.team_stat1.points, 1)
         self.assertEqual(self.team_stat2.points, 1)
+
+    def test_str(self):
+        match = MatchFactory()
+        self.assertEqual(match.__str__(), '{} vs {} ({}:{})'
+                         .format(match.team_home.team.name,
+                                 match.team_guest.team.name,
+                                 match.team_home_goals,
+                                 match.team_guest_goals))
+
+
+class LeagueModelCreateTestCase(TestCase):
+    def setUp(self):
+        self.league = LeagueFactory()
+
+    def test_league_create(self):
+        league = self.league
+
+        self.assertEqual(league.__str__(), 'TESTLEAGUE')
+        self.assertEqual(league.name, 'TESTLEAGUE')
+        self.assertEqual(league.shortcut, 'testleague')
+
+
+class TeamModelCreateTestCase(TestCase):
+    def setUp(self):
+        self.team = TeamFactory()
+
+    def test_team_create(self):
+        team = self.team
+
+        self.assertEqual(team.__str__(), 'Club: {}'.format(team.name))
+        self.assertEqual(team.name, '{}'.format(team.name))
+        self.assertEqual(team.shortcut, '{}'.format(team.shortcut))
+
+
+class TeamStatModelCreateTestCase(TestCase):
+    def setUp(self):
+        self.teamstat = TeamStatFactory()
+
+    def test_teamstat_create(self):
+        teamstat = self.teamstat
+
+        self.assertEqual(teamstat.points, 0)
+        self.assertEqual(teamstat.match_count, 0)
+        self.assertEqual(teamstat.wins, 0)
+        self.assertEqual(teamstat.loses, 0)
+        self.assertEqual(teamstat.draws, 0)
+        self.assertEqual(teamstat.goals_scored, 0)
+        self.assertEqual(teamstat.goals_conceded, 0)
+
+
+class PlayerModelCreateTestCase(TestCase):
+    def setUp(self):
+        self.player = PlayerFactory()
+
+    def test_player_create(self):
+        player = self.player
+        self.assertEqual(player.name, 'Rocko Pocko')
+        self.assertEqual(player.age, 21)
+
+    def test_player_str(self):
+        self.assertEqual(self.player.__str__(), '{} | Club: {}'
+                         .format(self.player.name, self.player.team.name))
+
+
